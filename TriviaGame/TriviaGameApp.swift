8c721cb499d15acbaee54ac46ae799b740a08e86
rniev <rniev027@fiu.edu>
2025-03-04 20:43:16 -0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Rafy Nieves on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriviaGameTests/TriviaGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameTests.swift
+//  TriviaGameTests
+//
+//  Created by Rafy Nieves on 3/4/25.
+//
+
+import Testing
+@testable import TriviaGame
+
+struct TriviaGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
